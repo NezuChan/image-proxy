6264d863846b382588c7dc72becf82a1414e9335
KagChi <59391215+KagChi@users.noreply.github.com>
2024-09-21 15:13:00 +0000

    feat: handle panic error

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/davidbyttow/govips/v2/vips"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
 	"github.com/nezuchan/image-proxy/impl/image_resizer/client"
@@ -24,7 +25,27 @@ func main() {
 	defer vips.Shutdown()
 
 	_ = godotenv.Load()
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		Prefork:      true,
+		ServerHeader: "Fiber",
+		ErrorHandler: func(ctx *fiber.Ctx, err error) error {
+			// Status code defaults to 500
+			code := fiber.StatusInternalServerError
+
+			// Retrieve the custom status code if it's a *fiber.Error
+			var e *fiber.Error
+			if errors.As(err, &e) {
+				code = e.Code
+			}
+
+			return ctx.Status(code).JSON(&struct_type.ErrorResponse{
+				Message: "Something went wrong, please try again later.",
+				Error:   err.Error(),
+			})
+		},
+	})
+
+	app.Use(recover.New())
 
 	app.Use(logger.New(logger.Config{
 		Format: "[${ip}]:${port} [${latency}] ${status} - ${method} ${path}\n",
